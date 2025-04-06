b01a10cffad6da7b95d56be549da868453841382
Kartik Kumar <ktk221102@gmail.com>
2025-04-07 01:14:20 +0530

    Initial Commit

---
+++ js/contact-script.js
@@ -0,0 +1,15 @@
+
+  document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_c72dcod', 'template_jd0vsfp', this)
+      .then(function(response) {
+         alert("Message sent successfully!");
+      }, function(error) {
+         alert("FAILED to send message.\n" + JSON.stringify(error));
+      });
+
+    // Optional: Reset the form
+    this.reset();
+  });
+
